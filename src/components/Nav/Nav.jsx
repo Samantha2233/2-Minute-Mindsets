c35c0845270596ac2220de06dda75f84d1f62e6b
Samantha <sami.shackelford@gmail.com>
2019-10-28 16:32:52 -0700

    implement nav component

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+// import { NavLink, Link } from 'react-router-dom';
+import './Nav.scss';
+
+const Nav = props => {
+    return (
+        <div className="nav">
+            <div className="logo-and-links">
+                <img className="logo" src="../../../images/logo-purple.png" alt="logo" />
+                <ul>
+                    <li>About</li>
+                    <li>Videos</li>
+                    <li>Subscribe</li>
+                </ul>
+            </div>
+            <div>
+                <img id="hamburger" src="../../../images/hamburger-purple.png" alt="mobile-menu icon" />
+                <img id="exit" src="../../../images/Exit-icon-purple.png" alt="exit mobile-menu icon" />
+            </div>
+        </div>
+    )
+}
+
+export default Nav;
