9857177369bc36eaf3efb489737468cfb5fc9111
Samantha <sami.shackelford@gmail.com>
2019-10-29 16:56:42 -0700

    Update Nav & Favicon

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -9,13 +9,16 @@ const Nav = props => {
                 <img className='logo' src="../../../images/logo-purple.png" alt='logo' />
                 <ul>
                     <li>About</li>
-                    <li>Videos</li>
+                    <li>Resources</li>
                     <li>Subscribe</li>
                 </ul>
             </div>
             <div>
                 <img id='hamburger' src='../../../images/hamburger-purple.png' alt='mobile-menu icon' />
                 <img id="exit" src='../../../images/Exit-icon-purple.png' alt='exit mobile-menu icon' />
+                {/* user ? icon : signUp */}
+                <p>Sign Up</p>
+                <i id='user-icon' class='far fa-user'></i>
             </div>
         </div>
     )
