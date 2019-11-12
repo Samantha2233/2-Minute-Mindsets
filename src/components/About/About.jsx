96fc25cc444d5e7f7a9578eed00a71fbfd638161
Samantha <sami.shackelford@gmail.com>
2019-11-11 20:09:05 -0800

    update about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,28 +3,31 @@ import './About.scss';
 
 const About = () => {
     return (
-        <div id="about">
-            <h2>What is 2 Minute Mindsets?</h2>
-            <h3>Common sense not commonly practiced.</h3>
+        <div>
+            <div id="about">
+                <h2 className="h2">What is 2 Minute Mindsets?</h2>
+                <h3 className="h3">Common sense not commonly practiced.</h3>
 
-            <p>Most of us want to live an extradinary life but we don’t get started because we think it takes too much work but it’s not hard work.
-                It’s <span class="purple">consistent work</span>.
-                It’s doing all the little things <span class="purple">better everyday</span>.
-                Your already thinking so why not think better? You are already doing things why not do them better?
-                When you <span class="purple">take the wisdom</span> of these concepts, overtime with consistent effort you can
-                <span class="purple">shift your mindset</span> live a life by design. A life with true accomplishment and no regrets. A deeply happy life.
-                Simple wisdom to guide you and <span class="purple">remind you just how to live a better life</span>.
-                What is 2MinuteMindsets? Common sense not commonly practiced. <span class="purple">Easy to implement concepts</span> that are incredibly practical and relevant right now in your life.
-                How will it help you?
-                We can travel the world or read all of the ancient texts to gain wisdom but <span class="purple">great living comes from building the habit</span> of simple ideas presented in a straight forward way. Follow these ideas and look for way to incorporate them in your life and you will have results that will exceeed your expectations.
-                What do you need to do?
-                Watch the video.
-                Use it in your life.
-                Let us know what happened.
-                <span class="purple">Naturally build the habit of living better</span>.
+                <p>
+                    It’s <span className="purple">consistent work</span>.
+                It’s doing all the little things <span className="purple">better everyday</span>.
+                                Your already thinking so why not think better? You are already doing things why not do them better?
+                When you <span className="purple">take the wisdom</span> of these easy-to-implement concepts, overtime with consistent effort you can
+                <span className="purple"> shift your mindset </span> live a life by your design. A life with true accomplishment and no regrets. A deeply happy life.
+                Simple wisdom to guide you and <span className="purple">remind you just how to live a better life</span>.
+                What is 2MinuteMindsets? Common sense not commonly practiced. <span className="purple">Easy to implement concepts</span> that are incredibly practical and relevant right now in your life.
+                                How will it help you?
+                We can travel the world or read all of the ancient texts to gain wisdom but <span className="purple">great living comes from building the habit</span> of simple ideas presented in a straight forward way. Follow these ideas and look for way to incorporate them in your life and you will have results that will exceeed your expectations.
+                                What do you need to do?
+                                Watch the video.
+                                Use it in your life.
+                                Let us know what happened.
+                <span className="purple">Naturally build the habit of living better</span>.
             </p>
+            </div>
         </div>
     )
+
 }
 
 export default About;
