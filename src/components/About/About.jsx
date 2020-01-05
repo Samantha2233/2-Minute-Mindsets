da21eef6361fc1b3cdb27b6b6459206150cefde7
Samantha <sami.shackelford@gmail.com>
2020-01-05 11:42:03 -0800

    update about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,18 +9,16 @@ const About = () => {
                 <h3 className="h3">Practical wisdom, practiced daily.</h3>
 
                 <p>
-                    Your already thinking, so why not think better? You are already doing, so why not do better? When you take the wisdom of these easy to use concepts, overtime with consistent effort, you will shift your mindset to live life by design. A life of satisfaction and less regret. A deeply happy life.
+                    You're already thinking, so why not think better? You are already doing, so why not do better? When you take the wisdom of these easy to use concepts, overtime with consistent effort, you will shift your mindset to live life by design. A life of satisfaction and less regret. A deeply happy life.
                     </p>
                 <h3 className="h3">How will it help you?</h3>
                 <p>You can travel the world or read all of the ancient texts to gain wisdom but great living comes from building habits from small actionable ideas. Small ideas with big implications.</p>
                 <h3 className="h3">What does it take?</h3>
-                <p> Watch the videos. Use the ideas in your life. Report what happened.
-
+                <p> Watch the videos. Use the ideas in your life.
                     Naturally build the habit of living better.</p>
             </div>
         </div>
     )
-
 }
 
 export default About;
