c025d0875bf432b3fbe66fba7fb81aa98814a8df
Samantha <sami.shackelford@gmail.com>
2020-01-08 16:46:39 -0800

    add console.log event and context

--- functions/serve.js
+++ functions/serve.js
@@ -1,6 +1,12 @@
-console.log('Does serve function work?');
 
 exports.handler = function (event, context, callback) {
+  console.log('Does serve function work?');
+
+  console.log('EVENT:', event);
+  console.log('CONTEXT:', context);
+
+  // if (event.httpMethod === 'POST' && event.path === )
+
   callback(null, {
     statusCode: 200,
     body: 'Hello?'
